e3f7c50aeed54bd4c7643c27c4ecf583bb542a8c
ucesnsa <nil.sari@gmail.com>
2022-06-26 19:21:22 +0100

    Initial implementation of Bus start and end location inference algorithm for LTDS complete

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -10,8 +10,30 @@ from mylib import data_model as dm
 
 # algorithm to infer bus info
 def infer_bus_info(journey_current,journey_next, journey_prev, user_detail, verbos):
-    journey_current.StartStationInferred = 'Halaluya'
-    journey_current.EndStationInferred = 'Yes Halaluya'
+    # for current_journey if previous_journey = None, then current_journey.start_location_inferred = Home_loc
+    # for current_journey if previous_journey.journey_type= rail or tube, then current_journey.start_location_inferred = previous_journey.end_station
+    # for current_journey if previous_journey.journey_type= bus, then current_journey.start_location_inferred = un_determined
+    if journey_prev == None :
+        journey_current.StartStationInferred = user_detail.HomeLocation
+    elif journey_prev.TransportMode != dm.TransportEnum.BUS :
+        journey_current.StartStationInferred = journey_prev.EndStation
+    elif journey_prev.TransportMode == dm.TransportEnum.BUS :
+        journey_current.StartStationInferred = 'Undetermined'
+    else:
+        journey_current.StartStationInferred = 'None'
+
+    # for current_journey if next_journey = None, then current_journey.end_location_inferred = Home_location
+    # for current_journey if next_journey.journey_type= rail or tube, then current_journey.end_location_inferred = next_journey.start_station
+    # for current_journey if next_journey.journey_type=bus, then current_journey.end_location_inferred = un_determined
+    if journey_next == None :
+        journey_current.EndStationInferred = user_detail.HomeLocation
+    elif journey_next.TransportMode != dm.TransportEnum.BUS :
+        journey_current.EndStationInferred = journey_next.StartStation
+    elif journey_next.TransportMode == dm.TransportEnum.BUS :
+        journey_current.EndStationInferred = 'Undetermined'
+    else:
+        journey_current.EndStationInferred = 'None'
+
     return journey_current
 
 #verbos - use 1 for debugging and 0 for normal run
@@ -44,49 +66,42 @@ for index, row in dfDays.iterrows():
     dfJourneys_by_date = pd.DataFrame.copy(dfJourneys.loc[(dfJourneys['date_key'] == process_date)])
     dfJourneys_by_date.reset_index(inplace=True)
     #loop through all the journeys on the selected date
-    print ('Date',len(dfJourneys_by_date))
     for index, row in dfJourneys_by_date.iterrows():
+        #print (row)
         journey_current = mj.convert_to_Journey(row, verbos=0)
 
         if index+1 < len(dfJourneys_by_date) -1:
-            next_index = index+1
+            next_index = index + 1
         else:
             next_index = -1
 
-        if index - 1 < 0 :
+        if index - 1 >= 0 :
             prev_index = index - 1
         else:
             prev_index = -1
 
         if journey_current.TransportMode == dm.TransportEnum.BUS.name:
-            print ('current journey Index', index, 'Next Journey Index:', next_index, 'Prev Journey Index:', prev_index)
-            journey_next = None
-            journey_prev = None
+            #print('length', len(dfJourneys_by_date))
+            #print('Index printing', dfJourneys_by_date.index.tolist())
+            #print ('current journey Index', index, 'Next Journey Index:', next_index, 'Prev Journey Index:', prev_index)
+
+            if prev_index != -1:
+                #print(dfJourneys_by_date.iloc[prev_index])
+                journey_prev = mj.convert_to_Journey(dfJourneys_by_date.iloc[prev_index], verbos=0)
+            else:
+                journey_prev = None
+
+            if next_index != -1:
+                #print(dfJourneys_by_date.iloc[next_index])
+                journey_next = mj.convert_to_Journey(dfJourneys_by_date.iloc[next_index], verbos=0)
+            else:
+                journey_next = None
+
             journey_current = infer_bus_info(journey_current,journey_next,journey_prev,usr,verbos)
-            print (journey_current)
+            #print (journey_current)
 
         # add new journey to the list
         lstJourneys.append(journey_current)
-
-#create a new journey object
-#journey_prev = dm.Journey('A', 'B', dm.TransportEnum.TUBE, '', '', '', '')
-#journey_next = dm.Journey('A', 'B', dm.TransportEnum.TUBE, '', '', '', '')
-
-
-
-# 4. apply bus journey inference algorithm
-# select the 1st bus journey as current_journey, and two additional journeys if available (previous_journey and next_journey)
-# for current_journey if previous_journey = None, then current_journey.start_location_inferred = Home_loc
-# for current_journey if previous_journey.journey_type= rail or tube, then current_journey.start_location_inferred = previous_journey.end_station
-# for current_journey if previous_journey.journey_type= bus, then current_journey.start_location_inferred = un_determined
-
-# for current_journey if next_journey = None, then current_journey.end_location_inferred = Home_location
-# for current_journey if next_journey.journey_type= rail or tube, then current_journey.end_location_inferred = next_journey.start_station
-# for current_journey if next_journey.journey_type=bus, then current_journey.end_location_inferred = un_determined
-
-# select the next bus journey as current_journey, and two additional journeys if available (previous_journey and next_journey)
-# continue till no more bus journeys available on the day
-
 
 dfJourneyDataFinal = pd.DataFrame(lstJourneys)
 #print (dfJourneyDataFinal)
@@ -101,5 +116,3 @@ dfJourneyDataFinal.to_excel(writer)
 # save the excel
 writer.save()
 print("DataFrame is exported successfully to Excel File.")
-
-
