0013d4e42c0f70b0c294a3720a6ee9cdc9fdf217
ucesnsa <nil.sari@gmail.com>
2022-09-22 13:13:31 +0100

    added the Postgres DB query to gather the relevent stats into the project

---
+++ StatsAnalysis/results query ( infer and valid) stats.sql
@@ -0,0 +1,84 @@
+﻿-- inference by user class - using home location 
+--------------------------------------------------------
+-- user break down by user class 
+-- journey by user class and algorithm rule 
+-- main output table : "shenzhen_bus_inference"
+with inf as (
+select "UserId" as userid,a."TransportMode" ,
+case 
+ when "EndStationInferred" like '%(NextRule)' then 'NextRule' 
+ when "EndStationInferred" like '%(Stage1)' then 'Stage1' 
+ when "EndStationInferred" like '%(Stage2)' then 'Stage2' 
+ when "EndStationInferred" like '%(Stage3)' then 'Stage3' 
+end rule_name, b.user_class
+--,b.userid, * 
+from "shenzhen_bus_inference" a 
+left join shenzhen_user_class b on a."UserId" = b.userid 
+)
+--where b.userid is null 
+select "TransportMode", user_class, rule_name, count(*) from inf
+group by "TransportMode", user_class, rule_name
+
+
+--------------------------------------
+-- validations 
+-- journey by user class and algorithm rule 
+-- main output table : "shenzhen_bus_valid"
+with val as (
+select "UserId" as userid,a."TransportMode" ,"EndStationInferred" ,
+case 
+ when "ValidEndStationInferred" like '%(NextRule)' then 'NextRule' 
+ when "ValidEndStationInferred" like '%(Stage1)' then 'Stage1' 
+ when "ValidEndStationInferred" like '%(Stage2)' then 'Stage2' 
+ when "ValidEndStationInferred" like '%(Stage3)' then 'Stage3' 
+end rule_name, b.user_class
+--,b.userid, * 
+from "shenzhen_bus_valid" a 
+left join shenzhen_user_class b on a."UserId" = b.userid 
+)
+--where b.userid is null 
+select "TransportMode", user_class, rule_name, count(*) from val
+group by "TransportMode", user_class, rule_name
+order by "TransportMode", user_class, rule_name
+
+
+----------------- MISC queries ----------------------
+------------------------------------------------- 
+--6,302,666
+select count(*) from  shenzhen_user_class 
+
+
+-- 200,071
+-- output table for inference 
+select * from shenzhen_bus_inference
+
+--129,326
+-- output table for validation 
+select * from shenzhen_bus_valid
+limit 1000
+
+--- 4,162,374
+select count(*) from  shenzhen_user_class
+where user_class not in ('Adult;Elderly','Not Useful','Not Required','Adult;Student')
+
+
+-- user break down by user class 
+SELECT user_class, count(*) FROM public.shenzhen_users a
+left join shenzhen_user_class b on a.User_Id = b.userid 
+where string_agg not in ('2') 
+group by user_class
+
+-- users and home location by user journey type
+SELECT * FROM public.shenzhen_users a 
+inner join public.user_info_infer b on a.user_id = b.UserId 
+where b.homeFoundCount > 3 
+limit 10 
+
+-- valid by user class - using home location
+------------------------------------------------------------------
+SELECT user_class, count(*) FROM public.shenzhen_users a
+left join shenzhen_user_class b on a.User_Id = b.userid 
+where string_agg not in ('2') 
+group by user_class
+
+
